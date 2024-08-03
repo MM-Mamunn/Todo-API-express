fd08d2494f545d1cffc18d1b11b8c2b9a2a51c30
MM-Mamunne <gamesmamun8@gmail.com>
2024-08-03 20:24:37 +0600

    modified and front end link

--- models/Task.js
+++ models/Task.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-const TaskSchema=new mongoose.Schema(
-    {
-        desc:{type: String, required: true},
-        isDone:{type:Boolean,default:false}
-    },
-    {
-        timestamps:true
-    }
+const TaskSchema = new mongoose.Schema(
+  {
+    idd: { type: String, required: true },
+    desc: { type: String, required: true },
+    isDone: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-export const Task = mongoose.model('Task', TaskSchema);
+export const Task = mongoose.model("Task", TaskSchema);
